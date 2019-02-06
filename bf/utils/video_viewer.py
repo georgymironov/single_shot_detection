8547abb7cfe8e447d4cd1baf088294fb508a5353
Georgy Mironov <george.mironov@synesis.ru>
2019-02-06 18:12:34 +0300

    Small refactor

--- bf/utils/video_viewer.py
+++ bf/utils/video_viewer.py
@@ -15,20 +15,20 @@ def _is_video(f):
 
 class VideoViewer(object):
     def __init__(self, path, predictor):
-        self.path = path
+        path = path.replace('file://', '')
+
+        if os.path.isdir(path):
+            self.paths = [x for x in glob.glob(os.path.join(path, '**'), recursive=True) if _is_video(x)]
+        else:
+            self.paths = [path]
+
         self.predictor = predictor
 
     def run(self):
-        path = self.path.replace('file://', '')
-
-        if os.path.isdir(path):
-            paths = [x for x in glob.glob(os.path.join(path, '**'), recursive=True) if _is_video(x)]
-        else:
-            paths = [path]
-
         stop = False
-        for path in paths:
+        for path in self.paths:
             print(path)
+
             cap = cv2.VideoCapture(path)
             cv2.namedWindow('image')
 
@@ -36,12 +36,7 @@ class VideoViewer(object):
                 _, frame = cap.read()
                 rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-                ratio_w = rgb.shape[1] / self.predictor.resize.size[0]
-                ratio_h = rgb.shape[0] / self.predictor.resize.size[1]
-
                 prediction = self.predictor.predict_single(rgb)
-                prediction[..., [0, 2]] *= ratio_w
-                prediction[..., [1, 3]] *= ratio_h
 
                 result = dataset_utils.display(rgb, prediction)
 
--- detection/detector_wrapper.py
+++ detection/detector_wrapper.py
@@ -10,6 +10,9 @@ class DetectorWrapper(object):
         self.resize = resize
 
     def predict_single(self, input_):
+        ratio_w = input_.shape[1] / self.resize.size[0]
+        ratio_h = input_.shape[0] / self.resize.size[1]
+
         with bf.preprocessing.set_transform_type('no_target'):
             if self.resize is not None:
                 input_ = self.resize(input_)
@@ -17,6 +20,12 @@ class DetectorWrapper(object):
                 input_ = self.preprocess(input_)
         if input_.dim() == 3:
             input_ = input_.unsqueeze(0)
+
         *prediction, priors = self.model(input_.to(self.device))
         prediction = [x.detach() for x in prediction]
-        return self.postprocessor.postprocess(prediction, priors)[0]
+        result = self.postprocessor.postprocess(prediction, priors)[0]
+
+        result[..., [0, 2]] *= ratio_w
+        result[..., [1, 3]] *= ratio_h
+
+        return result
