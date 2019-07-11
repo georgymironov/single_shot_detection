5224ea7757f75c16dac902c201ccd13fa2a20197
Georgy Mironov <george.mironov@synesis.ru>
2019-07-11 17:21:43 +0300

    Add weight fixer rule

--- detection/utils/model_fixer/rules.py
+++ detection/utils/model_fixer/rules.py
@@ -8,6 +8,10 @@ def register(func):
     return func
 
 @register
+def add_predictor(weights):
+    return OrderedDict({k if k.startswith('predictor.') else f'predictor.{k}': v for k, v in weights.items()})
+
+@register
 def rename_class_to_score(weights):
     return OrderedDict({k.replace('.class.', '.score.'): v for k, v in weights.items()})
 
